a3006521f8ed9239050a02a6d6f1ff2ee8008d1f
MSulSal <129910316+MSulSal@users.noreply.github.com>
2025-02-19 02:44:47 -0600

    add video stream

--- my-new-project/src/index.js
+++ my-new-project/src/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/video", async (req, res) => {
+    const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+    const stats = await fs.promises.stat(videoPath);
+
+    res.writeHead(200, {
+        "Content-Length": stats.size,
+        "Content-Type": "video/mp4"
+    });
+    fs.createReadStream(videoPath).pipe(res);
 });
 
 app.listen(port);
